d4e59f7c76dded7e866ad6d0b7b413dce38fe8c2
chenguofan1999 <chenguofan1999@outlook.com>
2020-09-24 18:08:20 +0800

    inLab

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "modules.c"
 
 int main()
@@ -9,5 +10,15 @@ int main()
     // unsigned long long T = finalPerm(t);
     // printf("%llu\n",T);
 
+    unsigned long long key = 0x7ABC7A62F3498EBC;
+
+    unsigned long long *subkeySet= (unsigned long long*) malloc(17 * sizeof(unsigned long long));
+
+    genSubKeys(key, subkeySet);
+
+    for(int i = 1; i <= 16; i++)
+    {
+        printf("k_%2d : %llu\n", i, subkeySet[i]);
+    }
 
 }
--- modules.c
+++ modules.c
@@ -1,5 +1,6 @@
-#include "dataStructures.h"
+#include "tables.h"
 
+//typedef unsigned long long block;
 unsigned long long perm(unsigned long long input, short table[], int n)
 {
     unsigned long long ret = 0;
@@ -68,3 +69,55 @@ unsigned long long PC2_perm(unsigned long long K56)
     return perm(K56, PC2, 48);
 }
 
+
+void genSubKeys(unsigned long long K, unsigned long long *subkeys)
+{   
+    unsigned long long C0D0 = PC1_perm(K);
+    unsigned long long C[17], D[17];
+    D[0] = (C0D0 << 36) >>36;
+    C[0] = C0D0 >> 28;
+
+    for(int i = 0; i <= 16; i++)
+    {
+        if(i == 1 || i == 2 || i == 9 || i == 16)
+        {
+            // left cyclic shift 1 bit
+
+            // record the highest bit
+            unsigned long long tc = C[i-1] >> 27;
+            // left shift 1 bit
+            C[i] = C[i-1] << 1;
+            // cp highest bit to lowest bit
+            C[i] = C[i] | tc;
+            // cut the 29th bit
+            C[i] = (C[i] << 36) >> 36;
+
+            // the same 
+            unsigned long long td = D[i-1] >> 27;
+            D[i] = D[i-1] << 1;
+            D[i] = D[i] | td;
+            D[i] = (D[i] << 36) >> 36;
+        }
+        else
+        {
+            // left cyclic shift 2 bits
+
+            // record the highest 2 bits
+            unsigned long long tc = C[i-1] >> 26;
+            // left shift 2 bits
+            C[i] = C[i-1] << 2;
+            // cp highest 2 bits to lowest 2 bits
+            C[i] = C[i] | tc;
+            // cut the 29th and 30th bits
+            C[i] = (C[i] << 36) >> 36;
+
+            //the same
+            unsigned long long td = D[i-1] >> 26;
+            D[i] = D[i-1] << 2;
+            D[i] = D[i] | td;
+            D[i] = (D[i] << 36) >> 36;
+        }
+        int concat = (C[i] << 28) | D[i];
+        subkeys[i] = PC2_perm(concat);
+    }
+}
